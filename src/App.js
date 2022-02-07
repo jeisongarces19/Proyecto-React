c0a60b9c7396a22a3a64b9a2ec2de236ae6bb075
jeisongarces19 <jeisongarces@javerianacali.edu.co>
2022-02-07 16:51:12 -0500

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import "./App.css";
 /*Todo esto de aqui es jsx o javascrit , no html */
 
 
-export default function Cabezera() {
+export default function Cabezera(props) {
   return (
     <div className='Cabezera' id='Titulo'>
-      <h1 id='Titulo'>Bienvenido a COLTEC!</h1>             
+      <h1 id='Color'>{props.texto}</h1>    
+      <h3 id='Subtitulo'>{props.subtitulo}</h3>      
     </div>
   );
 }
 
-/* Estas tres cosas son equivalentes para llamar componentes*/
-
+/* Propiedades props que son para cambiar cosas a los componente*/
 
 export default function App() {
   return (
     <div className='App'>
-      <p><Cabezera> Llamar un componente</Cabezera></p>
+      <p><Cabezera texto='Bienvenido a COLTEC!' subtitulo='Ingenieria SAS'> Llamar un componente</Cabezera></p>
+
+      <h3><Cabezera texto='Revision Diaria' > Llamar un componente usando un prop</Cabezera></h3>
 
       <header className='App-header'>
         <p>Por favor, Ingrese los datos:</p>        

51e5e1f74c37361301522d9cbac44fd63478553e
jeisongarces19 <jeisongarces@javerianacali.edu.co>
2022-02-08 16:58:36 -0500

    1 file modified

--- src/Coltec.js
+++ src/Coltec.js
@@ -4,55 +4,73 @@ import "./Coltec.css";
 /*Todo esto de aqui es jsx o javascrit , no html */
 
 /*Los datos internos de los componentes se llaman estados(stade) */
-export default function Cabezera(props) {
+export default function Ayuda(props) {
   return (
-    <div className='Cabezera' id='Titulo'>
-      <h1 id='Color'>{props.texto}</h1>    
-      <h3 id='Subtitulo'>{props.subtitulo}</h3>      
+    <div className='Ayuda' id='Titulo'>
+      <h1 id='Color'>{props.texto}</h1>         
     </div>
   );
 }
 
 
-class EnviarInfo extends React.Component{
+class Cabezera extends React.Component{
     //Los estados se manejan asi
     state = {
       show: true
-    }  
-    CambioEstado = () => {
-      //con el ! cambio los estados, si esta en false cmabia a true y biserversa
-      this.setState({show: !this.state.show})
     }
+  
     render(){
       if(this.state.show){
         return (    
           <div className='Cabezera' id='Titulo'>
             <h1>{this.props.texto}</h1> 
-            {this.props.subtitulo}
-            
-            <button onClick={this.CambioEstado}> Cambio de estado apagar objeto funcion</button>
-          
+            {this.props.subtitulo}                   
           </div>
         )
       }else{
-        return <h1>No hay elementos
-                  
-                </h1> 
+        return <h1>No hay elementos</h1> 
       }
     }
+}
+
+class DatosRegistro extends React.Component{
+  //Los estados se manejan asi
+  state = {
+    show: true
   }
+  render(){
+    if(this.state.show){
+      return (    
+        <div>
+          <p>{this.props.texto}</p> 
+          <input type="checkbox"/>
+        </div>
+      )
+    }else{
+      return <h1>No hay elementos</h1> 
+    }
+  }
+}
 
 /* Propiedades props que son para cambiar cosas a los componente*/
 
 export default function AppPrincipal() {
   return (
     <div>
-      <p><EnviarInfo texto='Bienvenido a COLTEC!' subtitulo='Ingenieria SAS'> Llamar un componente</EnviarInfo></p>
+      
+      <p><Cabezera texto='Bienvenido a COLTEC!' subtitulo='Ingenieria SAS'> Llamar un Cabezera</Cabezera></p>
 
 
       <header className='App-header'>
         <p>Por favor, Ingrese los datos:</p>        
       </header>
+
+      <p><DatosRegistro texto='Covid'> Llamar un </DatosRegistro></p>    <p><DatosRegistro texto='Gripa'> Llamar un </DatosRegistro></p>
+      <p><DatosRegistro texto='Algo'> Llamar un </DatosRegistro></p>
+      <p><DatosRegistro texto='Peligro'> Llamar un </DatosRegistro></p>
+
+
+
 
       <button> Enviar Informacion desde AppPrincipal </button>
 

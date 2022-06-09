9d8a8975f76f0134839c0acbe83de46e4822468d
Jeison Garces Castañeda <jeisongarces@javerianacali.edu.co>
2022-06-09 02:48:01 -0500

    Update EditarExposicionesVirtuales.js

--- src/Pages/EditarExposicionesVirtuales.js
+++ src/Pages/EditarExposicionesVirtuales.js
@@ -6,6 +6,14 @@ import '../App.css';
 import '../Styles/editarExposicion.css';
 
 import ReactPlayer from 'react-player';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
 import swal from 'sweetalert';
 

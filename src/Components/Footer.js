278348d929cd3f19577b260703b8aedc4c07e1d6
jeisongarces19 <jeisongarces@javerianacali.edu.co>
2022-06-10 02:16:19 -0500

    Actualizacion
    Se agrego la libreria del asistente virtual y se mejoro algunas cosas

---
+++ src/Components/Footer.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import "bulma/css/bulma.css";
+//import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.css';
+import '../App.css';
+import '../Styles/footer.css';
+
+
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      items: [], text: '' 
+    };
+    
+  }
+
+  render() {
+    return (
+
+      <center>
+
+        <footer className="footer">
+          <div className="container">
+            <nav>
+              <ul>
+                <li>
+                    <a href=" " target="_blank">
+                      Created for social foundations
+                    </a>
+
+                </li>
+
+                <span> </span>
+                
+                <li>
+                  <a href=" " target="_blank">
+                  MECA Project
+                  </a>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="copyright" id="copyright">
+              © 2022, Designed and Coded by  
+              <span> </span>
+              <a rel="noreferrer" href="https://www.linkedin.com/in/jeison-garces-casta%C3%B1eda-134b931b2/" target="_blank">
+                Jeison Garces
+              </a>
+              <span> </span>
+              and
+              <span> </span>
+              <a rel="noreferrer" href="https://www.linkedin.com/in/santiflo/" target="_blank">
+                Santiago Florian
+              </a>.
+            </div>
+          </div>
+        </footer>
+      </center>
+
+        
+    );
+  }
+
+  
+}
+
+
+
+export default Footer;
